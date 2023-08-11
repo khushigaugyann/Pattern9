bdbe0bbb1004ad535a62959576f6001cf1dca146
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-11 17:54:05 +0530

    Add files via upload

---
+++ Pattern9.java
@@ -0,0 +1,10 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+        for(int i=6;i>=1;i--){
+            for(int j=6;j>=i;j--){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
